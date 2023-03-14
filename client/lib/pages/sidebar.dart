75e7f201a6c5e53e636882a949cedae4a963d6e7
Jasper <jasper.de.bie1998@gmail.com>
2023-03-14 13:41:38 +0100

    added sidebar and dashboard classes

---
+++ client/lib/pages/sidebar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Sidebar extends StatefulWidget {
+  const Sidebar({Key? key}) : super(key: key);
+
+  @override
+  State<Sidebar> createState() => _SidebarState();
+}
+
+class _SidebarState extends State<Sidebar> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
