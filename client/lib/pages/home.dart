1cbb9976823397f8d1d4ad4d4a750f333fddeecf
Jasper <jasper.de.bie1998@gmail.com>
2023-03-14 13:29:15 +0100

    force app horizontal

--- client/lib/pages/home.dart
+++ client/lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -8,6 +9,15 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  @override
+  void initState(){
+    super.initState();
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.landscapeRight,
+      DeviceOrientation.landscapeLeft,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return const Text("Home");
