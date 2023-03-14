c6d49ea580885679beafd4abc9a6a30c279a7be9
Jasper <jasper.de.bie1998@gmail.com>
2023-03-14 13:26:38 +0100

    exported home to own file

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'pages/home.dart';
 
 void main() => runApp(MaterialApp(
     title: 'Deck1 Dispatch App',
@@ -34,9 +35,3 @@ class Loading extends StatelessWidget {
 
 
 
-class Home extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return const Text('Home');
-  }
-}
