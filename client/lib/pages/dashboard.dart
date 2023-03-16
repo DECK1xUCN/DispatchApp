7d21b93e32785102ca30075f61a03f2c89947436
ttommmy <93518302+ttommmy@users.noreply.github.com>
2023-03-16 13:47:00 +0100

    Line chart groundwork

--- client/lib/pages/dashboard.dart
+++ client/lib/pages/dashboard.dart
@@ -1,5 +1,7 @@
 import 'package:client/classes/CardContent.dart';
 import 'package:flutter/material.dart';
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/src/rendering/box.dart';
 
 import '../widgets/card_widget.dart';
 
@@ -13,6 +15,14 @@ class Dashboard extends StatefulWidget {
 class _SidebarState extends State<Dashboard> {
   String user = 'John Doe';
   String title = 'Airport Flight Officer';
+  List<FlSpot> spots = [
+    FlSpot(0,0),
+    FlSpot(1,5),
+    FlSpot(2,4),
+    FlSpot(3,3),
+    FlSpot(4,2),
+    FlSpot(5,1),
+  ];
 
   CardContent cardContent = CardContent(
       title: 'Dispatched flights',
@@ -40,7 +50,9 @@ class _SidebarState extends State<Dashboard> {
                         children: [
                           Text(
                             "Welcome, $user",
-                            style: TextStyle(fontSize: 40, color: Colors.black, fontWeight: FontWeight.bold),
+                            style: TextStyle(fontSize: 40,
+                                color: Colors.black,
+                                fontWeight: FontWeight.bold),
                           ),
                           Text(
                             title,
@@ -50,12 +62,63 @@ class _SidebarState extends State<Dashboard> {
                       ),
                     ),
                   ),
-                  const SizedBox(
-                    height: 200,
-                    child: Text(
-                        'Graph',
-                      style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold, color: Colors.black),
-                    ),
+                  Container( // TODO: Make more beutiful
+                    height: 100,
+                    child: LineChart(
+                        LineChartData(
+                          lineTouchData: LineTouchData(enabled: false),
+                          gridData: FlGridData(
+                            show: true,
+                            getDrawingHorizontalLine: (value) {
+                              return FlLine(
+                                color: Colors.grey,
+                                strokeWidth: 1,
+                              );
+                            },
+                            drawVerticalLine: true,
+                            getDrawingVerticalLine: (value) {
+                              return FlLine(
+                                color: Colors.grey,
+                                strokeWidth: 1,
+                              );
+                            },
+                          ),
+                          titlesData: FlTitlesData(
+                            show: true,
+                            bottomTitles: AxisTitles(
+                              axisNameWidget: Text("Bottom"),
+                              sideTitles: SideTitles(
+                                showTitles: true,
+                                getTitlesWidget: defaultGetTitle,
+                              ),
+                            ),
+                            leftTitles: AxisTitles(
+                              axisNameWidget: Text("Left"),
+                              sideTitles: SideTitles(
+                                showTitles: true,
+                                getTitlesWidget: defaultGetTitle,
+                              ),
+                            ),
+                          ),
+                          minX: 0,
+                          maxX: 20,
+                          minY: 0,
+                          maxY: 20,
+                          lineBarsData: [
+                            LineChartBarData(
+                              show: true,
+                              spots: spots,
+                              color: Colors.green,
+                              barWidth: 2,
+                              isCurved: true,
+
+                            ),
+                          ]
+                        ),
+
+
+
+                      ),
                   ),
                   const Text(
                     'Today',
