775caa069e8a017ca95536f8c81fa8100950a8e8
Jasper <jasper.de.bie1998@gmail.com>
2023-03-30 15:27:05 +0200

    replaced plutogrid with native listview

--- client/lib/pages/flights.dart
+++ client/lib/pages/flights.dart
@@ -1,5 +1,6 @@
+import 'package:client/classes/Flight.dart';
 import 'package:flutter/material.dart';
-import 'package:pluto_grid/pluto_grid.dart';
+import 'package:intl/intl.dart';
 
 class Flights extends StatefulWidget {
   const Flights({Key? key}) : super(key: key);
@@ -9,79 +10,131 @@ class Flights extends StatefulWidget {
 }
 
 class _FlightsState extends State<Flights> {
-  final List<PlutoColumn> columns = <PlutoColumn>[
-    PlutoColumn(
-      title: 'ETD',
-      field: 'etd',
-      type: PlutoColumnType.time(),
-      width: 200,
-      enableEditingMode: false,
-    ),
-    PlutoColumn(
-      title: 'Flightnumber',
-      field: 'flightnumber',
-      type: PlutoColumnType.text(),
-      width: 100,
-      enableEditingMode: false,
-    ),
-    PlutoColumn(
-      title: 'From',
-      field: 'from',
-      type: PlutoColumnType.text(),
-      width: 100,
-      enableEditingMode: false,
-    ),
-    PlutoColumn(
-      title: 'Via',
-      field: 'via',
-      type: PlutoColumnType.text(),
-      width: 100,
-      enableEditingMode: false,
-    ),
-    PlutoColumn(
-      title: 'To',
-      field: 'to',
-      type: PlutoColumnType.text(),
-      width: 100,
-      enableEditingMode: false,
-    ),
+  List<Flight> flights = [
+    Flight(
+        etd: DateTime.now().add(const Duration(hours: 1)),
+        flightnumber: 'ABC-1',
+        from: 'A',
+        via: ['B', 'C'],
+        to: 'D'),
+    Flight(
+        etd: DateTime.now().add(const Duration(hours: 2)),
+        flightnumber: 'ABC-2',
+        from: 'A',
+        via: ['B', 'C'],
+        to: 'D'),
+    Flight(
+        etd: DateTime.now().add(const Duration(hours: 3)),
+        flightnumber: 'ABC-3',
+        from: 'A',
+        via: ['B', 'C'],
+        to: 'D'),
+    Flight(
+        etd: DateTime.now().add(const Duration(hours: 4)),
+        flightnumber: 'ABC-4',
+        from: 'A',
+        via: ['B', 'C'],
+        to: 'D'),
   ];
 
-  final List<PlutoRow> rows = [
-    PlutoRow(
-      cells: {
-        'etd': PlutoCell(value: DateTime.now()),
-        'flightnumber': PlutoCell(value: '1234-A'),
-        'from': PlutoCell(value: 'ABC'),
-        'via': PlutoCell(value: 'V1'),
-        'to': PlutoCell(value: 'DEF'),
-      },
-    ),
-  ];
-
-  late final PlutoGridStateManager stateManager;
+  List<Widget> generateRows() {
+    List<Widget> rows = [];
+    for (var element in flights) {
+      rows.add(
+          GestureDetector(
+            onTap: (){
+              print('tapped' + element.flightnumber.toString());
+            },
+            child: Container(
+                height: 30,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        child: Center(child: Text(DateFormat.Hm().format(element.etd))),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Center(child: Text(element.flightnumber)),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Center(child: Text(element.from)),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Center(child: Text(element.via.toString())),
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        child: Center(child: Text(element.to)),
+                      ),
+                    ),
+                  ],
+                )
+            ),
+          )
+      );
+    }
+    return rows;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[800],
       body: SafeArea(
         child: Column(
           children: [
-            Text('potential add flight button'),
             Expanded(
-              child: PlutoGrid(
-                columns: columns,
-                rows: rows,
-                onLoaded: (PlutoGridOnLoadedEvent event) {
-                  stateManager = event.stateManager;
-                },
-                onChanged: (PlutoGridOnChangedEvent event) {
-                  print(event);
-                },
-                configuration: const PlutoGridConfiguration(),
-              ),
+              child: ListView(children: <Widget>[
+                Container(
+                  height: 30,
+                  decoration: const BoxDecoration(
+                    border: Border(
+                      bottom: BorderSide(width: 1.0, color: Colors.black),
+                    ),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Expanded(
+                        child: Container(
+                          child: const Center(child: Text('ETD', style: TextStyle(fontWeight: FontWeight.bold),)),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          child: const Center(child: Text('Flight number', style: TextStyle(fontWeight: FontWeight.bold),)),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          child: const Center(child: Text('From', style: TextStyle(fontWeight: FontWeight.bold),)),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          child: const Center(child: Text('Via', style: TextStyle(fontWeight: FontWeight.bold),)),
+                        ),
+                      ),
+                      Expanded(
+                        child: Container(
+                          child: const Center(child: Text('To', style: TextStyle(fontWeight: FontWeight.bold),)),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                ...generateRows()
+              ]),
             ),
-            Text('space for generating dfr button')
+            const Text('space for generating dfr button')
           ],
         ),
       ),
