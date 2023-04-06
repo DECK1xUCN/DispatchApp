ee97be61dd7ca02a8b70ff13a58f3d06c30ed482
Tomáš Sabol <tomassabol2@gmail.com>
2023-04-06 12:46:12 +0200

    added docs for pilot

--- server/src/graphql/resolvers/pilots.ts
+++ server/src/graphql/resolvers/pilots.ts
@@ -4,6 +4,17 @@ import { createGraphQLError } from "graphql-yoga";
 
 const pilotResolver = {
   Query: {
+    /**
+     * Retrieves all pilots from the database using Prisma's pilot.findMany() method.
+     *
+     * @async
+     * @function
+     * @param {any} parent - The parent object.
+     * @param {any} args - Unused.
+     * @param {Context} context - The context object containing the Prisma client.
+     * @returns {Promise<Array<Object>>} A Promise that resolves to an array of pilot objects.
+     * @throws {GraphQLError} If no pilots are found, a GraphQLError with a 404 status code is thrown.
+     */
     pilots: async (parent: any, args: any, context: Context) => {
       const pilots = await context.prisma.pilot.findMany().catch((err: any) => {
         throw createGraphQLError(`No sites found`, {
@@ -21,6 +32,18 @@ const pilotResolver = {
       }
       return pilots;
     },
+
+    /**
+     * Retrieves a single pilot by ID from the database using Prisma's pilot.findUnique() method.
+     *
+     * @async
+     * @function
+     * @param {any} parent - The parent object.
+     * @param {{id: string}} args - An object containing the ID of the pilot to retrieve.
+     * @param {Context} context - The context object containing the Prisma client.
+     * @returns {Promise<Object>} A Promise that resolves to a pilot object.
+     * @throws {GraphQLError} If no pilot is found with the specified ID, a GraphQLError with a 404 status code is thrown.
+     */
     pilot: async (parent: any, args: { id: string }, context: Context) => {
       const pilot = await context.prisma.pilot
         .findUnique({
@@ -43,7 +66,20 @@ const pilotResolver = {
       return pilot;
     },
   },
+
   Mutation: {
+    /**
+     *Creates a new pilot with the given name.
+     * @async
+     * @function createPilot
+     * @param {Object} parent - The parent resolver's result.
+     * @param {Object} args - The arguments passed to the resolver.
+     * @param {Object} args.data - The input data for creating a new pilot.
+     * @param {string} args.data.name - The name of the new pilot.
+     * @param {Object} context - The context object containing the Prisma client.
+     * @throws {GraphQLError} Throws an error if the pilot could not be created.
+     * @returns {Object} Returns the newly created pilot object.
+     */
     createPilot: async (
       parent: any,
       args: { data: PilotInput },
@@ -71,6 +107,21 @@ const pilotResolver = {
       }
       return pilot;
     },
+
+    /**
+     * Updates the details of a specific pilot.
+     *
+     * @async
+     * @function updatePilot
+     * @param {Object} parent - The parent object in the resolver chain.
+     * @param {Object} args - The arguments passed to the resolver function.
+     * @param {string} args.id - The ID of the pilot to update.
+     * @param {Object} args.data - The updated details of the pilot.
+     * @param {string} args.data.name - The updated name of the pilot.
+     * @param {Object} context - The context object containing services such as the Prisma client.
+     * @returns {Promise<Object>} The updated pilot.
+     * @throws {Error} Throws an error if the pilot cannot be found or updated.
+     */
     updatePilot: async (
       parent: any,
       args: { id: string; data: PilotInput },
