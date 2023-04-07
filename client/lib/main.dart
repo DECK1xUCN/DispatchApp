0d3bf99a633976b00de519a1eb6eb6c474ede11d
Jasper <jasper.de.bie1998@gmail.com>
2023-04-07 13:57:54 +0200

    setup main for graphql

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,41 +1,59 @@
+import 'package:client/pages/flightForm.dart';
 import 'package:client/pages/loading.dart';
 import 'package:flutter/material.dart';
 import 'pages/home.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
 
-void main() => runApp(MaterialApp(
-        title: 'Deck1 Dispatch App',
-        theme: ThemeData(
-          // Define the default brightness and colors.
-          brightness: Brightness.dark,
-          primaryColor: Colors.lightBlue[800],
-          
-          // Define the default font family.
-          fontFamily: 'Roboto',
+void main() async {
+  await initHiveForFlutter();
 
-          // Define the default `decoration` for the Input fields
-          inputDecorationTheme: const InputDecorationTheme( // TODO: Further designing choices are needed
-            enabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.black),
+  final HttpLink httpLink = HttpLink(
+    'http://deck1.sk:4000/graphql',
+  );
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      link: httpLink,
+      cache: GraphQLCache(store: HiveStore()),
+    ),
+  );
+
+  runApp(GraphQLProvider(
+      client: client,
+      child: MaterialApp(
+          title: 'Deck1 Dispatch App',
+          theme: ThemeData(
+            // Define the default brightness and colors.
+            brightness: Brightness.dark,
+            primaryColor: Colors.lightBlue[800],
+
+            // Define the default font family.
+            fontFamily: 'Roboto',
+
+            // Define the default `decoration` for the Input fields
+            inputDecorationTheme: const InputDecorationTheme(
+              // TODO: Further designing choices are needed
+              enabledBorder: OutlineInputBorder(
+                borderSide: BorderSide(color: Colors.black),
+              ),
+              hintStyle: TextStyle(fontFamily: 'Hind', color: Colors.black),
             ),
-            hintStyle: TextStyle(fontFamily: 'Hind', color: Colors.black),
 
+            // Define the default `TextTheme`. Use this to specify the default
+            // text styling for headlines, titles, bodies of text, and more.
+            textTheme: const TextTheme(
+              displayLarge:
+                  TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
+              titleLarge: TextStyle(
+                  fontSize: 25.0, fontFamily: 'Hind', color: Colors.black),
+              bodyMedium: TextStyle(
+                  fontSize: 14.0, fontFamily: 'Hind', color: Colors.black),
+            ),
           ),
-
-          // Define the default `TextTheme`. Use this to specify the default
-          // text styling for headlines, titles, bodies of text, and more.
-          textTheme: const TextTheme(
-            displayLarge:
-                TextStyle(fontSize: 72.0, fontWeight: FontWeight.bold),
-            titleLarge: TextStyle(fontSize: 25.0, fontFamily: 'Hind', color: Colors.black),
-            bodyMedium: TextStyle(fontSize: 14.0, fontFamily: 'Hind', color: Colors.black),
-
-
-          ),
-        ),
-
-
-        initialRoute: '/',
-        routes: {
-          '/': (context) => Loading(),
-          '/home': (context) => Home(),
-        }));
+          initialRoute: '/',
+          routes: {
+            '/': (context) => Loading(),
+            '/home': (context) => Home(),
+            '/flightform': (context) => FlightForm(),
+          })));
+}
