3f76a41af3c5ca2d8860da2c6658ade7e83772c1
Tomáš Sabol <tomassabol2@gmail.com>
2023-04-08 10:41:55 +0200

    added sentry to backend

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,13 +4,23 @@ import { makeExecutableSchema } from "@graphql-tools/schema";
 import resolvers from "./graphql/resolvers";
 import typeDefs from "./graphql/typeDefs";
 import express from "express";
+import * as Sentry from "@sentry/node";
 
 function main() {
   const app = express();
+
+  // Create GraphQL schema
   const schema = makeExecutableSchema({
     resolvers: [resolvers],
     typeDefs: [typeDefs],
   });
+
+  // Initialize Sentry
+  Sentry.init({
+    dsn: "https://42a1f5475b854a11b48c8d13df244f73@o4504977322868736.ingest.sentry.io/4504977336696832",
+    tracesSampleRate: 1.0,
+  });
+
   const yoga = createYoga({ schema, context });
   app.use("/graphql", yoga);
   app.listen(4000, () => {
