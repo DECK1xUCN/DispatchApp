24a372b00c219751811c652f4379ee2e274b5dc6
Jasper <jasper.de.bie1998@gmail.com>
2023-04-10 15:59:41 +0200

    init flights

---
+++ client/lib/pages/sites.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Sites extends StatefulWidget {
+  const Sites({Key? key}) : super(key: key);
+
+  @override
+  State<Sites> createState() => _SitesState();
+}
+
+class _SitesState extends State<Sites> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
